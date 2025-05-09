6e7f40fcd3b89aa37344bd4c4282e765629b11fc
Mohd Arshad Khan <osndl.mohdak@aicofindia.com>
2025-05-09 14:32:19 +0530

    first push of drf user authentication

--- data_app/urls.py
+++ data_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .authentication import CustomTokenObtainPairSerializer
+from .views import UserListView, UserCreateView
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('register/', UserCreateView.as_view(), name='user-register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- data_app/views.py
+++ data_app/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import UserLogin
+from .serializers import UserLoginSerializer, UserCreateSerializer
+
+
+class UserListView(generics.ListAPIView):
+    queryset = UserLogin.objects.all()
+    serializer_class = UserLoginSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = UserLogin.objects.all()
+    serializer_class = UserCreateSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'id': user.pk,
+            'email': user.email
+        })
